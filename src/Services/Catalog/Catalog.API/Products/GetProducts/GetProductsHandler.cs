e8280f3963f6747e9340f76b96ecc51b72a64c45
Umut Oku <uoku@cyberportbilisim.com>
2024-10-19 15:50:17 +0300

    added cqrs design pattern

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    internal class GetProductsQueryHandler(IDocumentSession documentSession, ILogger<GetProductsQueryHandler> logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler called");
+
+            var products = await documentSession.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products); 
+        }
+    }
+}
